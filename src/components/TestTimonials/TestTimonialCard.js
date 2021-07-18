ce0cba02bb2c2f731c56a3cfe0ef5659ae052787
engineerscodes <practicalengineers05@gmail.com>
2021-07-19 02:33:25 +0530

    testimony addes

--- src/components/TestTimonials/TestTimonialCard.js
+++ src/components/TestTimonials/TestTimonialCard.js
@@ -3,50 +3,26 @@ import React from "react";
 import Carousel from "react-elastic-carousel";
 
 import   "./TestTimonialStyle.css"
+import axios from "axios";
 
 function TestTimonialCardComponent() {
+  const baseURL = `http://127.0.0.1:8000/BannerImg/testimony/`;
+  const [Testimony, setTestimony] = React.useState(null);
+
+  React.useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setTestimony(response.data);
+    });
+  }, []);
+  if (!Testimony) return null;
+   
   return (
-        <>
-         
-    <Carousel breakPoints={breakPoints}  className="TestMon"  >
-      
-     
-     <TsMainCard
-       
-       image="https://instagram.fpnq10-1.fna.fbcdn.net/v/t51.2885-15/e35/94103488_135002498095966_268268835189795825_n.jpg?_nc_ht=instagram.fpnq10-1.fna.fbcdn.net&_nc_cat=110&_nc_ohc=xouG0tcFaC8AX9dAtsA&tp=1&oh=f96c795a18540132812ac5e2512845ab&oe=604752F0"
-       
-     />
-      <TsMainCard
-       
-       image="https://instagram.fpnq10-1.fna.fbcdn.net/v/t51.2885-15/e35/95427079_2199938276818767_8264144520212908448_n.jpg?_nc_ht=instagram.fpnq10-1.fna.fbcdn.net&_nc_cat=103&_nc_ohc=7df1VxOq7VAAX-ESoDr&tp=1&oh=e23a8dea5077e188062b7b2a36af91da&oe=60452368"
-       
-     />
-          
-
-           <TsMainCard
-       
-       image="https://instagram.fpnq10-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/95986435_238755844042100_567441978249453056_n.jpg?_nc_ht=instagram.fpnq10-1.fna.fbcdn.net&_nc_cat=108&_nc_ohc=UXuNQTKmekkAX_pB66M&tp=1&oh=012828de8e8cfa4a74a0a0972abe4841&oe=604581FF"
-       
-     />
-     
-
-      <TsMainCard
-       
-       image="https://instagram.fpnq10-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/96266353_2319347718368334_8273669043165148598_n.jpg?_nc_ht=instagram.fpnq10-1.fna.fbcdn.net&_nc_cat=106&_nc_ohc=l26l0qwDaZwAX_t2LcZ&tp=1&oh=5facd50a6af9aa907909a560d0ffca69&oe=60466F9C"
-       
-     />
-      <TsMainCard
-       
-        image="https://instagram.fpnq10-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/100997177_232110611576985_6773837881081980995_n.jpg?_nc_ht=instagram.fpnq10-1.fna.fbcdn.net&_nc_cat=109&_nc_ohc=XwmlLhjIZxoAX_EgzQX&tp=1&oh=83169d8cad6c05038d9d803040e9936f&oe=60461780"
-        
-      />
-     
-    
-    
-
-     
-
-    </Carousel>
+    <>
+      <Carousel breakPoints={breakPoints} className="TestMon">
+        {Testimony.data.map((data, index) => (
+          <TsMainCard image={data.TestimonialImgUrl} key={index} testimony={data.testimony} author={data.ByName}/>
+        ))}
+      </Carousel>
     </>
   );
 }
@@ -59,7 +35,7 @@ const TsMainCard = (props) => (
                   </div>
 
                   <div  className="TS2">
-                       <p>“One of the best clubs out there to hone your skills.” Cpt. Jack Sparrow</p>
+                       <p>{props.testimony } <span>{props.author}</span></p>
                   </div>
             </div>
         </>

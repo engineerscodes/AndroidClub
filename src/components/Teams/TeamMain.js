c0d6c9b9f61ac1bb585874bb757dc309a2f5cdfc
engineerscodes <practicalengineers05@gmail.com>
2021-07-19 00:50:35 +0530

    get current core team

--- src/components/Teams/TeamMain.js
+++ src/components/Teams/TeamMain.js
@@ -5,42 +5,44 @@ import ButtonRouterLink from "../ReactRouterBtn/ButtonRouterLink";
 
 import React from "react";
 import axios from "axios";
-function TeamMain(){
-    
-    const baseURL = "http://127.0.0.1:8000/team/";
-    const [post, setPost] = React.useState(null);
-    
-    React.useEffect(() => {
-      axios.get(baseURL).then((response) => {
-        setPost(response.data);
-      });
-    }, []);
-    if (!post) return null;
+function TeamMain() {
+  const baseURL = `http://127.0.0.1:8000/team/${new Date().getFullYear()}`;
+  const [post, setPost] = React.useState(null);
 
-    return (
-      <>
-        <DisplayList id="team">
-          <DisplayH1> CORE TEAM 2020-2021 </DisplayH1>
-        </DisplayList>
-        <DisplayList>
-          <Teamgrid>
-            {post.data.map((data, index) => (
-              <div key={index}>
-                <Teams src={data.img_url} name={data.name} pbio={data.bio} flink={data.fallow}  />
-              </div>
-            ))}
-          </Teamgrid>
-        </DisplayList>
+  React.useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setPost(response.data);
+    });
+  }, []);
+  if (!post) return null;
 
-        <DisplayList>
-          <ButtonRouterLink gotoPage={"/AndroidClub"} textdes={"Home"} />
-        </DisplayList>
-      </>
-    );
+  return (
+    <>
+      <DisplayList id="team">
+        <DisplayH1> CORE TEAM 2020-2021 </DisplayH1>
+      </DisplayList>
+      <DisplayList>
+        <Teamgrid>
+          {post.data.map((data, index) => (
+            <div key={index}>
+              <Teams
+                src={data.img_url}
+                name={data.name}
+                pbio={data.bio}
+                flink={data.fallow}
+              />
+            </div>
+          ))}
+        </Teamgrid>
+      </DisplayList>
 
+      <DisplayList>
+        <ButtonRouterLink gotoPage={"/AndroidClub"} textdes={"Home"} />
+      </DisplayList>
+    </>
+  );
 }
 export default TeamMain;
-
 
 /*
 

232fafa2a42a7edcf535349a99439f416ebbdecc
engineerscodes <practicalengineers05@gmail.com>
2021-07-19 15:19:15 +0530

    speaker api added

--- src/components/Speakers/Speakers.js
+++ src/components/Speakers/Speakers.js
@@ -1,8 +1,28 @@
 import React from 'react'
 import './SpeakerStyle.css' 
+import axios from "axios";
 
 const Speakers = () =>
  {
+      const baseURL = `http://127.0.0.1:8000/events/alert/`;
+      const [Speaker, setSpeaker] = React.useState(null);
+
+      React.useEffect(() => {
+        axios.get(baseURL).then((response) => {
+          setSpeaker(response.data);
+        });
+      }, []);
+      if (!Speaker) return null;
+      const links = Speaker.data.events.RegisterLink;
+      function flinkp(link) {
+        if(link ==="")
+        {
+          console.log("Wait for link !!!")
+        }
+        else{
+        window.location.href = link;
+        }
+      }
     return (
       <>
         <div className="SPEAKERCONSTAINER" id="announcement">
@@ -10,18 +30,24 @@ const Speakers = () =>
             <div className="spk1">
               <img
                 className="spk1img"
-                src="https://im.rediff.com/money/2014/sep/19sundar4.jpg"
-                alt="Speaker's"
+                src={Speaker.data.speakers.SpkImg}
+                alt={Speaker.data.speakers.speakerName.toUpperCase()}
               />
             </div>
 
             <div className="spk2">
               <h1 className="spk2h1">EVENT ALERT </h1>
-              <h2 className="spk2h2"> Speaker Name</h2>
-              <h2 className="spk2h2">Designation</h2>
-              <h3 className="spk2h3">EVENT DATE</h3>
+              <h2 className="spk2h2">
+                {Speaker.data.speakers.speakerName.toUpperCase()}
+              </h2>
+              <h2 className="spk2h2">
+                {Speaker.data.speakers.Designation.toUpperCase()}
+              </h2>
+              <h3 className="spk2h3">
+                {Speaker.data.events.EventDate.toUpperCase()}
+              </h3>
 
-              <button className="SpeakerButton">
+              <button className="SpeakerButton" onClick={() => flinkp(links)}>
                 Get Your FREE TICKERS HERE
               </button>
             </div>

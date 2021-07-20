e8162268b29821382e133fdcccccf3e6eb598575
engineerscodes <practicalengineers05@gmail.com>
2021-07-20 18:11:00 +0530

    cleaned eventpage

--- src/RouterPages/EventsPage.js
+++ src/RouterPages/EventsPage.js
@@ -3,16 +3,7 @@ import React, { Component } from "react";
 import Navbar from "../components/NavBar/NavBar";
 
 import SideBar from "../components/SlideBar/sideBar";
-import Banner from "../components/Banner/Banner";
-import About from "../components/About/About";
-import { AboutInfo } from "../components/About/infoAbout";
 import Footers from "../components/Footer/Footers";
-
-import InfoSectionArea from "../INFOSECTION/InfoSectionArea";
-import EventCardGrids from "../components/EventCardGrids/EventCardGrids";
-import CardComponent from "../components/Cards/CardComponent";
-import TestTimonialCardComponent from "../components/TestTimonials/TestTimonialCard";
-import Speakers from "../components/Speakers/Speakers";
 import Events from '../components/EventsList/EventList';
 
 class EventsPage extends Component {
@@ -29,6 +20,7 @@ class EventsPage extends Component {
         <Navbar toggle={this.toggle} />
          <Events/>
         <div>
+
           <Footers />
         </div>
       </>
